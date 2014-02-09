b307950fbab938cbb1a93bcef377e770979fd8da
Santino-Wu <wsq_bill@126.com>
2014-02-10 00:44:02 +0800

    Project initialization 01

--- Doc/test.sql
+++
@@ -1,3 +0,0 @@
-/** 插入一条用户信息 */
-/** 6d84604cc54214eca821d90237d150b2 */
-INSERT INTO `stn_user`(`username`, `password`, `e_mail`) VALUES('santinocom', md5('santinocom'), 'santinocom@localhost.com');
